6cc91314053f3bc5dca5c09966f4465887972e7d
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-22 13:22:02 +0100

    Add project files.

---
+++ MvcCorePracticas/Controllers/MatematicasController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCorePracticas.Controllers
+{
+    public class MatematicasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SumarNumerosGet(int numero1, int numero2)
+        {
+            int suma = numero1 + numero2;
+            ViewData["MENSAJE"] = numero1 + " + " + numero2 + " = " + suma;
+            return View();
+        }
+
+        public IActionResult SumarNumerosPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SumarNumerosPost(int cajanumero1, int cajanumero2)
+        {
+            int suma = cajanumero1 + cajanumero2;
+            ViewData["MENSAJE"] = cajanumero1 + " + " + cajanumero2 + " = " + suma;
+            return View();
+        }
+
+    }
+}
